f7b29ae3b5627c2d47a00177bd0391efca8b92ab
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-08-23 14:06:30 +0200

    Added footer and header to the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import {Routes, Route} from "react-router-dom";
-
 import PageStart from "./pages/PageStart.tsx";
 import PageGenre from "./pages/PageGenre.tsx";
 import PageMovie from "./pages/PageMovie.tsx";
 import PageActor from "./pages/PageActor.tsx";
 import PageSearch from "./pages/PageSearch.tsx";
 import PageNotFound from "./pages/PageNotFound.tsx";
+import Header from "./components/Header/Header.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
 function App() {
 
   return (
-      <Routes>
-          <Route path="/" element={<PageStart />} />
-          <Route path="/movies/:genre" element={<PageGenre />} />
-          <Route path="/movie/:id" element={<PageMovie />} />
-          <Route path="/actor/:id" element={<PageActor />} />
-          <Route path="/search/" element={<PageSearch />} />
-          <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <div className="container lg">
+          <Header />
+          <Routes>
+              <Route path="/" element={<PageStart />} />
+              <Route path="/movies/:genre" element={<PageGenre />} />
+              <Route path="/movie/:id" element={<PageMovie />} />
+              <Route path="/actor/:id" element={<PageActor />} />
+              <Route path="/search/" element={<PageSearch />} />
+              <Route path="*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+      </div>
   )
 }
 
