9090b7cda8abfa335e2af8a89a7652f143627a3e
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-08-24 09:41:53 +0200

    Added button and functionality to show and hide genres

--- src/components/Sections/BrowseGenres.tsx
+++ src/components/Sections/BrowseGenres.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import {useFetchGenre} from "../../hooks/useFetchGenre.ts";
-// import {genresInt} from "../../interfaces/genres.interface.ts";
+import {Button} from "@nextui-org/react";
 import FetchError from "../Error/FetchError.tsx";
 
 interface IProp {
@@ -9,16 +9,28 @@ interface IProp {
 
 const BrowseGenres:React.FC<IProp> = ({title}) => {
 
+    const [showAllGenres, setShowAllGenres] = useState(false)
+
     const genres = useFetchGenre()
-    console.log(genres)
+
+    const displayedGenres = showAllGenres ? genres.data?.genres : genres.data?.genres.slice(0,6)
+
+    const toggleShowGenres = () => {
+        setShowAllGenres(!showAllGenres)
+    }
+
+    console.log("reLoad?")
 
     return(
         <div>
             { genres.isError ? <FetchError /> : null }
             <h2>{title}</h2>
-            { genres.isSuccess && genres.data.genres.map((genre) => (
-                <div key={genre.id}>{genre.name}</div>
-            ))}
+            <div className="grid gap-2 grid-cols-2 grid-rows-2">
+                { genres.isSuccess && displayedGenres && displayedGenres.map((genre) => (
+                    <Button size="sm" key={genre.id}>{genre.name}</Button>
+                ))}
+            </div>
+            <Button onClick={toggleShowGenres}>{showAllGenres ? "Hide genres" : "Show all genres"}</Button>
         </div>
     )
 }
