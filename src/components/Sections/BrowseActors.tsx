f3a5dde914611054bbb47b1abcae09959b094b4a
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-08-25 11:31:44 +0200

    Rendering related actors on the movie page

--- src/components/Sections/BrowseActors.tsx
+++ src/components/Sections/BrowseActors.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {useFetchRelatedActors} from "../../hooks/useFetchRelatedActors.ts";
 import {Avatar} from "@nextui-org/react";
+import {FaCamera} from 'react-icons/fa'
 
 
 interface IProp {
@@ -11,16 +12,28 @@ interface IProp {
 const BrowseActors:React.FC<IProp> = ({id, title}) => {
 
     const allActors = useFetchRelatedActors(id)
-    const displayActors = allActors.data!.cast.slice(0,15)
-
-    console.log("All Actors: ", allActors)
-    console.log("Selected actors: ", displayActors)
+    const displayActors = allActors.data?.cast.slice(0,15)
 
     return(
         <div>
             <h2>{title}</h2>
-            Related actor ID: {id}
-
+            <div className="flex overflow-x-auto gap-x-6">
+                { displayActors && displayActors.map(actor => (
+                    <div key={actor.id} className="flex flex-col  gap-4 items-center">
+                        <Avatar
+                            className="w-28 h-28 text-large"
+                            name={actor.name}
+                            src={`https://image.tmdb.org/t/p/w300${actor.profile_path}`}
+                            showFallback
+                            fallback={
+                                <FaCamera/>
+                            }
+                        />
+                        <span>{actor.name}</span>
+                        <span>{actor.character}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
