b264e860cc2fb3d4f4e73b9f5fc8acc52057b689
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-08-28 09:31:26 +0200

    Adding a useEffect to verify that data is true. Will add this to other components/sections that use the same concept.

--- src/pages/PageMovie.tsx
+++ src/pages/PageMovie.tsx
@@ -4,11 +4,21 @@ import GridMovies from "../sections/GridMovies.tsx";
 import FetchError from "../components/Error/FetchError.tsx";
 import GridActors from "../sections/GridActors.tsx";
 import {useFetchMovieActor} from "../hooks/useFetchMovieActor.ts";
+import GridButtons from "../components/Buttons/GridButtons.tsx";
+import {genreInt} from "../interfaces/genres.interface.ts";
+import {useEffect, useState} from "react";
 
 const PageMovie = () => {
 
     const { id } = useParams()
     const movieQuery = id ? useFetchMovieActor(id, "movie") : null
+
+    const [movieGenres, setMovieGenres] = useState<genreInt[] | null>(null)
+
+    useEffect(() => {
+        if(movieQuery != null && movieQuery.data)
+            setMovieGenres(movieQuery.data.genres)
+    }, [movieQuery]);
 
     console.log("movie data: ", movieQuery!.data)
 
@@ -34,16 +44,14 @@ const PageMovie = () => {
                             <div><span>{movieQuery.data.runtime} min</span></div>
                         </div>
                         <div>
-                            { movieQuery.data.genres.map(movieGenre => (
-                                <span key={movieGenre.id}>{movieGenre.name}</span>
-                            ))}
-                        </div>
-                        <div>
                             <a href={`https://www.imdb.com/title/${movieQuery.data.imdb_id}/`} aria-label={`Visit ${movieQuery.data.title} on IMDB`} target="_blank">Visit {movieQuery.data.title} on IMDB</a>
                         </div>
                     </div>
                     <div>
                         {movieQuery.data.overview}
+                    </div>
+                    <div>
+                        <GridButtons data={movieGenres}/>
                     </div>
                     <div>
                         <GridActors title={"Actors"} id={id ? id.toString() : null} />
