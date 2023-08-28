525f001f85b733e23b6b6eede83f6d0b00470b8a
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-08-28 09:42:16 +0200

    Using useEffect to set data passed down to the component.

--- src/sections/MenuGenres.tsx
+++ src/sections/MenuGenres.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useFetchGenre} from "../hooks/useFetchGenre.ts";
 import {Button} from "@nextui-org/react";
 import FetchError from "../components/Error/FetchError.tsx";
 import GridButtons from "../components/Buttons/GridButtons.tsx";
+import {genreInt} from "../interfaces/genres.interface.ts";
 
 interface IProp {
     title: string
@@ -11,9 +12,15 @@ interface IProp {
 const MenuGenres:React.FC<IProp> = ({title}) => {
 
     const [showAllGenres, setShowAllGenres] = useState(false)
+    const [displayGenres, setDisplayGenres] = useState<genreInt[] | null>(null)
 
     const genres = useFetchGenre()
-    const displayedGenres = showAllGenres ? genres.data?.genres : genres.data?.genres.slice(0,6)
+
+    useEffect(() => {
+        if (genres.data) {
+            setDisplayGenres(genres.data.genres.slice(0,6))
+        }
+    }, [genres.data]);
 
     const toggleShowGenres = () => {
         setShowAllGenres(!showAllGenres)
@@ -23,7 +30,7 @@ const MenuGenres:React.FC<IProp> = ({title}) => {
         <div>
             { genres.isError ? <FetchError /> : null }
             <h2>{title}</h2>
-            { genres.isSuccess && <GridButtons data={displayedGenres}/> }
+            { genres.isSuccess && <GridButtons data={displayGenres}/> }
             <Button onClick={toggleShowGenres}>{showAllGenres ? "Hide genres" : "Show all genres"}</Button>
         </div>
     )
