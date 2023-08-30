b766f1eac6c5e785160fc694b25cd0b24ff12a1c
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-08-30 09:05:04 +0200

    Header with searchfield. onClick on button switches SearchField between visible and hidden.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
 import {Link} from "react-router-dom";
 import {FaSearch} from "react-icons/fa";
+import SearchField from "./SearchField.tsx";
+import {useState} from "react";
+import { Button } from '@nextui-org/react';
 
 const Header = () => {
+
+    const [isVisible, setIsVisible] = useState(false)
+
+    const handleVisible = () => {
+        setIsVisible(!isVisible)
+    }
+
     return (
         <div className="flex relative justify-between h-16 items-center bg-primary pl-4 pr-4">
             <div><Link to="/">HEADER LOGO</Link></div>
-            <div><FaSearch /></div>
+            <div>
+                <Button onClick={handleVisible}><FaSearch /></Button>
+                { isVisible && <SearchField handleVisible={handleVisible}/> }
+            </div>
         </div>
     )
 }
--- src/components/Header/SearchField.tsx
+++ src/components/Header/SearchField.tsx
@@ -1,7 +1,17 @@
-const SearchField = () => {
+import { Button, Input } from '@nextui-org/react';
+import React from "react";
+
+interface IProp {
+    handleVisible: () => void
+}
+
+const SearchField:React.FC<IProp> = ({handleVisible}) => {
+
     return(
-        <div>
-            Searchfield
+        <div className="flex items-center px-2 h-full w-full absolute top-0 left-0 bg-secondary ">
+            <Input />
+            <Button className="ml-2"/>
+            <Button className="ml-2" onClick={handleVisible}/>
         </div>
     )
 }
