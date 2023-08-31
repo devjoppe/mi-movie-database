5d6a2edeb4649e1f32ee06d539cc185b26688160
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-08-31 12:24:28 +0200

    Added component for visited movies that are being fetch from local storage.

--- src/pages/PageStart.tsx
+++ src/pages/PageStart.tsx
@@ -2,6 +2,7 @@ import MenuGenres from "../sections/MenuGenres.tsx";
 import GridMovies from "../sections/GridMovies.tsx";
 import OptionButtons from "../components/Buttons/OptionButtons.tsx";
 import {useState} from "react";
+import GridRecentVisited from "../sections/GridRecentVisited.tsx";
 
 const PageStart = () => {
 
@@ -12,18 +13,22 @@ const PageStart = () => {
         setIsTrending(data)
     }
 
-   return (
-       <>
-           <MenuGenres title={"Browse by genre"}/>
-           <h2>Most recent</h2>
-           <GridMovies url={"movie/"} identifier={"now_playing"} option={null} useRelated={false} />
-           <h2>Trending</h2>
-           <OptionButtons handleOptionClick={handleOptionClick} data={options}/>
-           <GridMovies url={"trending/movie/"} identifier={isTrending} option={null} useRelated={false} />
-           <h2>Popular</h2>
-           <GridMovies url={"movie/"} identifier={"top_rated"} option={null} useRelated={false} />
-       </>
-   )
+    return (
+        <>
+            <MenuGenres title={"Browse by genre"}/>
+            <h2>Most recent</h2>
+            <GridMovies url={"movie/"} identifier={"now_playing"} option={null} useRelated={false} />
+            <h2>Trending</h2>
+            <OptionButtons handleOptionClick={handleOptionClick} data={options}/>
+            <GridMovies url={"trending/movie/"} identifier={isTrending} option={null} useRelated={false} />
+            <h2>Popular</h2>
+            <GridMovies url={"movie/"} identifier={"top_rated"} option={null} useRelated={false} />
+            <div>
+                <h3>My recent visited movies</h3>
+                <GridRecentVisited />
+            </div>
+        </>
+    )
 }
 
 export default PageStart
